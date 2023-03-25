838ebc21f010344c8aea9f49c8acccce6036b19f
sandro <sandro@gmail.com>
2023-03-25 11:45:14 +0400

    morch

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Variables
 const firstName = document.getElementById('firstName')
 const lastName = document.getElementById('lastName')
 const password = document.getElementById('password') 
@@ -7,10 +8,10 @@ const firstNameError = document.getElementById('firstNameError')
 const lastNameError = document.getElementById('lstError')
 const passError = document.getElementById('passError')
 const checkboxError = document.getElementById('policy')
-
+//Code
 button.addEventListener('click', ()=>{
-    firstName.value ==`` ? firstNameError.innerHTML = `Please fill out this field` : firstNameError.innerHTML = ``
-    lastName.value == '' ? lastNameError.innerHTML = `Please fill out this field` : lastNameError.innerHTML = ``
-    password.value == "" ? passError.innerHTML = `Please fill out this field` :  passError.innerHTML = ``
-    !checkbox.checked ? checkboxError.innerHTML = 'Please accept privacy policy' : checkboxError.innerHTML =``
+    firstName.value =='' ? firstNameError.innerHTML = 'Please fill out this field' : firstNameError.innerHTML = ''
+    lastName.value == '' ? lastNameError.innerHTML = 'Please fill out this field' : lastNameError.innerHTML = ''
+    password.value == '' ? passError.innerHTML = 'Please fill out this field' :  passError.innerHTML = ''
+    !checkbox.checked ? checkboxError.innerHTML = 'Please accept privacy policy' : checkboxError.innerHTML =''
 })
