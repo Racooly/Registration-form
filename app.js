d44bc4f8311dac5895d54e4257a737c1d9f05ca7
sandro <sandro@gmail.com>
2023-04-01 19:10:19 +0400

    finished

--- app.js
+++ app.js
@@ -8,10 +8,23 @@ const firstNameError = document.getElementById('firstNameError')
 const lastNameError = document.getElementById('lstError')
 const passError = document.getElementById('passError')
 const checkboxError = document.getElementById('policy')
+const eyeOpen = document.getElementById('eyeOpen')
+const eyeClose = document.getElementById('eyeClosed')
 //Code
 button.addEventListener('click', ()=>{
     firstName.value =='' ? firstNameError.innerHTML = 'Please fill out this field' : firstNameError.innerHTML = ''
     lastName.value == '' ? lastNameError.innerHTML = 'Please fill out this field' : lastNameError.innerHTML = ''
     password.value == '' ? passError.innerHTML = 'Please fill out this field' :  passError.innerHTML = ''
     !checkbox.checked ? checkboxError.innerHTML = 'Please accept privacy policy' : checkboxError.innerHTML =''
+})
+
+eyeOpen.addEventListener('click', ()=>{
+    eyeClose.style.display = "inline"
+    eyeOpen.style.display = "none"
+    password.type = "text"
+})
+eyeClose.addEventListener('click', ()=>{
+    eyeOpen.style.display = "inline"
+    eyeClose.style.display = "none"
+    password.type = "password"
 })
